6f1425644e746d90c8d1c1c90c68c213ff7935bb
Nehal <nehal.er7@gamil.com>
2021-10-18 23:38:09 +0530

    Initial commit

---
+++ 13_pr_05.py
@@ -0,0 +1,26 @@
+
+class Train:
+    def __init__(self, name, fare, seats,):
+        self.name = name
+        self.fare = fare
+        self.seats = seats
+    
+    def Status(self):
+        print(f"The name of the train is: {self.name}")
+        print(f"The seats available in the train is: {self.seats}")
+    
+    def fareInfo(self):
+        print(f"The ticket of the train is : {self.fare}")
+
+
+
+fare = {"Mumbai":"400", "Latur":"150", "Pune":"250", "Hyderabad":"300"}
+seats = {"Mumbai":"60", "Latur":"100", "Pune":"150", "Hyderabad":"90"}
+n = input("Enter the trian name: ")
+n = n.capitalize()
+
+Express = Train(n,(fare[f"{n}"]),(seats[f"{n}"]))
+Express.Status()
+Express.fareInfo()
+
+
